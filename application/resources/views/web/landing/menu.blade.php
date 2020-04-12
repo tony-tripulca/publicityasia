b689da894078edcbd9ae6d853d747498959262c1
tony <tony.tripulca@gmail.com>
2020-04-13 01:47:20 +0800

    #38 Done with services and network

--- application/resources/views/web/landing/menu.blade.php
+++ application/resources/views/web/landing/menu.blade.php
@@ -68,6 +68,7 @@
         background-position: right center;
         background-repeat: no-repeat;
     }
+
     #services-content .list-group {
         margin: 0;
         padding: 0;
@@ -79,10 +80,10 @@
         background-color: transparent;
     }
     .carousel-control-prev {
-        left: -40px;
+        left: -20px;
     }
     .carousel-control-next {
-        right: -40px;
+        right: -20px;
     }
     #connect-content {
         background-image: url(../../images/connect.jpg);
@@ -110,6 +111,12 @@
         .menu.content {
             padding: 0 50px;
         }
+        .carousel-control-prev {
+            left: -40px;
+        }
+        .carousel-control-next {
+            right: -40px;
+        }
     }
     @media (min-height: 720px) {
         #network-content #article-carousel {
@@ -125,18 +132,14 @@
         #profile-content {
             font-size: 1.2rem;
         }
-    
+        #services-content {
+            font-size: 1.2rem;
+        }
+        #network-content #article-carousel {
+            margin-top: 35%;
+        }
+    }
 </style>
-@endsection
-
-@section('plugin-scripts')
-
-@endsection
-
-@section('module-scripts')
-<script type="text/javascript">
-    function toggleMenu(offset,label,content){$("html, body").animate({scrollTop:offset}),$(label).toggleClass("active"),$(content).toggleClass("active"),$(content).find(".lead-control").toggleClass("active")}function openMenu(offset,label,content){setTimeout((function(){$("html, body").animate({scrollTop:offset}),$(label).addClass("active"),$(content).addClass("active"),$(content).find(".lead-control").addClass("active")}),1e3)}function closeAllMenu(control,index){for(let i=0;i<control.label.length;i++)i!=index&&($(control.label[i]).removeClass("active"),$(control.content[i]).removeClass("active"),$(control.content[i]).find(".lead-control").removeClass("active"))}$(()=>{var control={menu:$(".menu.item"),label:$(".menu.item h1"),content:$(".menu.content"),offset:[$("#profile").offset().top-52,$("#services").offset().top-52,$("#network").offset().top-52,$("#originals").offset().top-52,$("#experiential").offset().top-52,$("#connect").offset().top-52],index:0},_articles=$("#network-content .carousel-inner"),url="";for(let i=0;i<15;i++){switch(i){case 0:url="https://www.hollywoodreporter.com/news/netflix-acquires-filipino-rom-girl-allergic-wifi-1229889";break;case 1:url="https://variety.com/2019/tv/asia/hbo-asia-renews-halfworlds-third-season-series-in-philippines-1203419572/";break;case 2:url="https://decider.com/2018/04/09/amo-netflix-review/";break;case 3:url="https://screenanarchy.com/2016/11/seclusion-trailer-in-erik-matti.html";break;case 4:url="https://www.economist.com/asia/2015/11/21/she-once-was-lost";break;case 5:url="https://www.imdb.com/news/ni62702733";break;case 6:url="https://tv5.espn.com/story/_/id/28258103/sibol-dota-2-team-gives-ph-another-gold-medal-esports";break;case 7:url="https://onenews.ph/phl-esports-bets-seen-to-lead-in-sea-games";break;case 8:url="https://news.abs-cbn.com/sports/12/08/19/sea-games-pinoy-mobile-legends-team-wins-first-esports-gold";break;case 9:url="https://www.gmanetwork.com/news/showbiz/chikaminute/704503/rocco-nacino-topbills-new-movie-write-about-love/story/";break;case 10:url="https://www.rappler.com/entertainment/music/244519-yeng-constantino-ikaw-ang-akin-write-about-love";break;case 11:url="https://sports.inquirer.net/379065/uncanny-heroes";break;case 12:url="https://www.preview.ph/beauty/5-derma-approved-ways-to-look-like-a-star-adv-con-pr";break;case 13:url="https://www.spin.ph/life/guide/sea-games-viewing-guide-a1374-20191119";break;case 14:url="https://www.esquiremag.ph/money/capital/c-estates-is-poised-to-democratize-the-real-estate-market-a1926-20190716"}var index,href;$(_articles).append(`<div class="carousel-item ${0==i?"active":""}">\n\t\t\t\t<a href=${href=url} target="_blank">\n\t\t\t\t\t<img src="{{ asset('images/articles/a${index=i}.jpg') }}" width="80%" alt="...">\n\t\t\t\t</a>\n\t\t\t</div>`)}$(control.menu).on("click",(function(event){control.index=control.menu.index(this),closeAllMenu(control,control.index),3!=control.index&&toggleMenu(control.offset[control.index],control.label[control.index],control.content[control.index])}))}),$(window).on("load",(function(){var urlParams;switch(new URLSearchParams(window.location.search).get("page")){case"profile":openMenu($("#profile").offset().top-52,$("#profile h1"),$("#profile-content"));break;case"services":openMenu($("#services").offset().top-52,$("#services h1"),$("#services-content"));break;case"network":openMenu($("#network").offset().top-52,$("#network h1"),$("#network-content"));break;case"originals":openMenu($("#originals").offset().top-52,$("#originals h1"),$("#originals-content"));break;case"experiential":openMenu($("#experiential").offset().top-52,$("#experiential h1"),$("#experiential-content"));break;case"connect":openMenu($("#connect").offset().top-52,$("#connect h1"),$("#connect-content"));break;default:$(window).scrollTop(0)}}));
-</script>
 @endsection
 
 @section('content')
@@ -159,9 +162,31 @@
     <div id="services" class="menu item bg-gray-2 d-flex align-items-center">
         <h1 class="ftra-medium text-25 text-lg-30 text-red">SERVICES</h1>
     </div>
-    <div id="services-content" class="menu content bg-nero ftra-heavy text-16 text-xl-20 line-15 space-10 text-justify text-white">
+    <div id="services-content" class="menu content bg-nero ftra-heavy text-lg-justify text-white">
         <div class="row no-gutters h-100 d-flex align-items-center">
-            <div class="col-12 col-lg-5 lead-control">
+            <div class="col-12 d-lg-none lead-control">
+                <ul class="list-group">
+                    <li class="list-group-item">Media Relations</li>
+                    <li class="list-group-item">Celebrity Acquisition</li>
+                    <li class="list-group-item">Creative & Strategy</li>
+                    <li class="list-group-item">Campaign Management</li>
+                    <li class="list-group-item">Digital & Media Placements</li>
+                    <li class="list-group-item">Design & Layout Services</li>
+                    <li class="list-group-item">Copywriting & Editing</li>
+                    <li class="list-group-item">Digital & Social Media</li>
+                    <li class="list-group-item">Event & Activation</li>
+                    <li class="list-group-item">International PR</li>
+                    <li class="list-group-item">Influencer Selection</li>
+                    <li class="list-group-item">Public Imaging</li>
+                    <li class="list-group-item">Executive Visibility</li>
+                    <li class="list-group-item">Thought Leadership</li>
+                    <li class="list-group-item">Original Production</li>
+                    <li class="list-group-item">Partnerships & Integration</li>
+                    <li class="list-group-item">Crisis Strategy & Response</li>
+                    <li class="list-group-item">Other PR Services</li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-5 d-none d-lg-block lead-control">
                 <ul class="list-group">
                     <li class="list-group-item">Media Relations</li>
                     <li class="list-group-item">Celebrity Acquisition</li>
@@ -174,7 +199,7 @@
                     <li class="list-group-item">Event & Activation</li>
                 </ul>
             </div>
-            <div class="col-12 col-lg-5 lead-control">
+            <div class="col-12 col-lg-5 d-none d-lg-block lead-control">
                 <ul class="list-group">
                     <li class="list-group-item">International PR</li>
                     <li class="list-group-item">Influencer Selection</li>
@@ -246,4 +271,14 @@
     </div>
     <div id="placeholder" class="bg-nero"></div>
 </section>
+@endsection
+
+@section('plugin-scripts')
+
+@endsection
+
+@section('module-scripts')
+<script type="text/javascript">
+    function toggleMenu(offset,label,content){$("html, body").animate({scrollTop:offset}),$(label).toggleClass("active"),$(content).toggleClass("active"),$(content).find(".lead-control").toggleClass("active")}function openMenu(offset,label,content){setTimeout((function(){$("html, body").animate({scrollTop:offset}),$(label).addClass("active"),$(content).addClass("active"),$(content).find(".lead-control").addClass("active")}),1e3)}function closeAllMenu(control,index){for(let i=0;i<control.label.length;i++)i!=index&&($(control.label[i]).removeClass("active"),$(control.content[i]).removeClass("active"),$(control.content[i]).find(".lead-control").removeClass("active"))}$(()=>{var control={menu:$(".menu.item"),label:$(".menu.item h1"),content:$(".menu.content"),offset:[$("#profile").offset().top-52,$("#services").offset().top-52,$("#network").offset().top-52,$("#originals").offset().top-52,$("#experiential").offset().top-52,$("#connect").offset().top-52],index:0},_articles=$("#network-content .carousel-inner"),url="";for(let i=0;i<15;i++){switch(i){case 0:url="https://www.hollywoodreporter.com/news/netflix-acquires-filipino-rom-girl-allergic-wifi-1229889";break;case 1:url="https://variety.com/2019/tv/asia/hbo-asia-renews-halfworlds-third-season-series-in-philippines-1203419572/";break;case 2:url="https://decider.com/2018/04/09/amo-netflix-review/";break;case 3:url="https://screenanarchy.com/2016/11/seclusion-trailer-in-erik-matti.html";break;case 4:url="https://www.economist.com/asia/2015/11/21/she-once-was-lost";break;case 5:url="https://www.imdb.com/news/ni62702733";break;case 6:url="https://tv5.espn.com/story/_/id/28258103/sibol-dota-2-team-gives-ph-another-gold-medal-esports";break;case 7:url="https://onenews.ph/phl-esports-bets-seen-to-lead-in-sea-games";break;case 8:url="https://news.abs-cbn.com/sports/12/08/19/sea-games-pinoy-mobile-legends-team-wins-first-esports-gold";break;case 9:url="https://www.gmanetwork.com/news/showbiz/chikaminute/704503/rocco-nacino-topbills-new-movie-write-about-love/story/";break;case 10:url="https://www.rappler.com/entertainment/music/244519-yeng-constantino-ikaw-ang-akin-write-about-love";break;case 11:url="https://sports.inquirer.net/379065/uncanny-heroes";break;case 12:url="https://www.preview.ph/beauty/5-derma-approved-ways-to-look-like-a-star-adv-con-pr";break;case 13:url="https://www.spin.ph/life/guide/sea-games-viewing-guide-a1374-20191119";break;case 14:url="https://www.esquiremag.ph/money/capital/c-estates-is-poised-to-democratize-the-real-estate-market-a1926-20190716"}var index,href;$(_articles).append(`<div class="carousel-item ${0==i?"active":""}">\n\t\t\t\t<a href=${href=url} target="_blank">\n\t\t\t\t\t<img src="{{ asset('images/articles/a${index=i}.jpg') }}" width="80%" alt="...">\n\t\t\t\t</a>\n\t\t\t</div>`)}$(control.menu).on("click",(function(event){control.index=control.menu.index(this),closeAllMenu(control,control.index),3!=control.index&&toggleMenu(control.offset[control.index],control.label[control.index],control.content[control.index])}))}),$(window).on("load",(function(){var urlParams;switch(new URLSearchParams(window.location.search).get("page")){case"profile":openMenu($("#profile").offset().top-52,$("#profile h1"),$("#profile-content"));break;case"services":openMenu($("#services").offset().top-52,$("#services h1"),$("#services-content"));break;case"network":openMenu($("#network").offset().top-52,$("#network h1"),$("#network-content"));break;case"originals":openMenu($("#originals").offset().top-52,$("#originals h1"),$("#originals-content"));break;case"experiential":openMenu($("#experiential").offset().top-52,$("#experiential h1"),$("#experiential-content"));break;case"connect":openMenu($("#connect").offset().top-52,$("#connect h1"),$("#connect-content"));break;default:$(window).scrollTop(0)}}));
+</script>
 @endsection
