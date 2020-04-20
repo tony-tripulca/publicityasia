03b2b969b811a949d6a6cfe1def41685b981d408
tony <tony.tripulca@gmail.com>
2020-04-21 03:02:42 +0800

    #38 Code refactor on media queries, done on services section

--- application/resources/views/web/landing/menu.blade.php
+++ application/resources/views/web/landing/menu.blade.php
@@ -50,19 +50,20 @@
 	display: none;
 }
 .menu.content.active {
-	height: calc(100% - 92px);
+	height: calc(100% - 100px);
 }
 .lead-control.active {
 	display: block;
 }
-#profile-content {
+#profile-content,
+#services-content {
 	font-size: 0.9rem;
 }
 #services-content {
 	background-image: url("{{ asset('images/services.jpg') }}");
 	background-size: cover;
 	background-position: right center;
-	background-repeat: no-repeat;
+    background-repeat: no-repeat;
 }
 #services-content .list-group {
 	margin: 0;
@@ -128,7 +129,10 @@
 	}
 	.menu.content {
 		padding: 0 50px;
-	}
+    }
+    .menu.content.active {
+        height: calc(100% - 144px);
+    }
 	#profile-content {
 		font-size: 1.3rem;
 		letter-spacing: 1.5px;
@@ -199,20 +203,30 @@
 @media (pointer: none), (pointer: coarse) and (min-width: 359px) {
 	#profile-content {
 		font-size: 1.03rem;
-	}
+    }
+    #services-content {
+        font-size: 1.06rem;
+    }
 }
 @media (pointer: none), (pointer: coarse) and (min-width: 374px) {
 	#profile-content {
 		font-size: 1.08rem;
-	}
+    }
+    #services-content {
+        font-size: 1.1rem;
+    }
 }
 @media (pointer: none), (pointer: coarse) and (min-width: 374px) and (min-height: 810px) {
 	#profile-content {
 		font-size: 1.2rem;
-	}
+    }
+    #services-content {
+        font-size: 1.3rem;
+    }
 }
 @media (pointer: none), (pointer: coarse) and (min-width: 410px) {
-	#profile-content {
+	#profile-content,
+	#services-content {
 		font-size: 1.2rem;
 	}
 }
@@ -337,6 +351,116 @@
 
 @section('module-scripts')
 <script type="text/javascript">
-    function getCurrentMedia(){return window.matchMedia("(min-width: 992px)").matches?"desktop":window.matchMedia("(min-width: 768px)").matches?"tablet":"mobile"}function toggleMenu(offset,label,content){$("html, body").animate({scrollTop:offset}),$(label).toggleClass("active"),$(content).toggleClass("active"),$(content).find(".lead-control").toggleClass("active")}function openMenu(offset,label,content){setTimeout((function(){$("html, body").animate({scrollTop:offset}),$(label).addClass("active"),$(content).addClass("active"),$(content).find(".lead-control").addClass("active")}),1e3)}function closeAllMenu(control,index){for(let i=0;i<control.label.length;i++)i!=index&&($(control.label[i]).removeClass("active"),$(control.content[i]).removeClass("active"),$(control.content[i]).find(".lead-control").removeClass("active"))}$(()=>{var control={menu:$(".menu.item"),label:$(".menu.item h1"),content:$(".menu.content"),offset:[$("#profile").offset().top-52,$("#services").offset().top-52,$("#network").offset().top-52,$("#originals").offset().top-52,$("#experiential").offset().top-52,$("#connect").offset().top-52],index:0},_articles=$("#network-content .carousel-inner"),url="";for(let i=0;i<15;i++){switch(i){case 0:url="https://www.hollywoodreporter.com/news/netflix-acquires-filipino-rom-girl-allergic-wifi-1229889";break;case 1:url="https://variety.com/2019/tv/asia/hbo-asia-renews-halfworlds-third-season-series-in-philippines-1203419572/";break;case 2:url="https://decider.com/2018/04/09/amo-netflix-review/";break;case 3:url="https://screenanarchy.com/2016/11/seclusion-trailer-in-erik-matti.html";break;case 4:url="https://www.economist.com/asia/2015/11/21/she-once-was-lost";break;case 5:url="https://www.imdb.com/news/ni62702733";break;case 6:url="https://tv5.espn.com/story/_/id/28258103/sibol-dota-2-team-gives-ph-another-gold-medal-esports";break;case 7:url="https://onenews.ph/phl-esports-bets-seen-to-lead-in-sea-games";break;case 8:url="https://news.abs-cbn.com/sports/12/08/19/sea-games-pinoy-mobile-legends-team-wins-first-esports-gold";break;case 9:url="https://www.gmanetwork.com/news/showbiz/chikaminute/704503/rocco-nacino-topbills-new-movie-write-about-love/story/";break;case 10:url="https://www.rappler.com/entertainment/music/244519-yeng-constantino-ikaw-ang-akin-write-about-love";break;case 11:url="https://sports.inquirer.net/379065/uncanny-heroes";break;case 12:url="https://www.preview.ph/beauty/5-derma-approved-ways-to-look-like-a-star-adv-con-pr";break;case 13:url="https://www.spin.ph/life/guide/sea-games-viewing-guide-a1374-20191119";break;case 14:url="https://www.esquiremag.ph/money/capital/c-estates-is-poised-to-democratize-the-real-estate-market-a1926-20190716"}var index,href;$(_articles).append(`<div class="carousel-item ${0==i?"active":""}">\n\t\t\t\t<a href=${href=url} target="_blank">\n\t\t\t\t\t<img src="{{ asset('images/articles/a${index=i}.jpg') }}" width="80%" alt="...">\n\t\t\t\t</a>\n\t\t\t</div>`)}$(control.menu).on("click",(function(event){control.index=control.menu.index(this),closeAllMenu(control,control.index),3!=control.index&&toggleMenu(control.offset[control.index],control.label[control.index],control.content[control.index])}))}),$(window).on("load",(function(){var urlParams;switch(new URLSearchParams(window.location.search).get("page")){case"profile":openMenu($("#profile").offset().top-52,$("#profile h1"),$("#profile-content"));break;case"services":openMenu($("#services").offset().top-52,$("#services h1"),$("#services-content"));break;case"network":openMenu($("#network").offset().top-52,$("#network h1"),$("#network-content"));break;case"originals":openMenu($("#originals").offset().top-52,$("#originals h1"),$("#originals-content"));break;case"experiential":openMenu($("#experiential").offset().top-52,$("#experiential h1"),$("#experiential-content"));break;case"connect":openMenu($("#connect").offset().top-52,$("#connect h1"),$("#connect-content"));break;default:$(window).scrollTop(0)}}));
+function getCurrentMedia() {
+	return window.matchMedia('(min-width: 992px)').matches ? 'desktop' : window.matchMedia('(min-width: 768px)').matches ? 'tablet' : 'mobile';
+}
+function toggleMenu(offset, label, content) {
+	$('html, body').animate({ scrollTop: offset }), $(label).toggleClass('active'), $(content).toggleClass('active'), $(content).find('.lead-control').toggleClass('active');
+}
+function openMenu(offset, label, content) {
+	setTimeout(function () {
+		$('html, body').animate({ scrollTop: offset }), $(label).addClass('active'), $(content).addClass('active'), $(content).find('.lead-control').addClass('active');
+	}, 1e3);
+}
+function closeAllMenu(control, index) {
+	for (let i = 0; i < control.label.length; i++) i != index && ($(control.label[i]).removeClass('active'), $(control.content[i]).removeClass('active'), $(control.content[i]).find('.lead-control').removeClass('active'));
+}
+$(() => {
+	var control = {
+        menu: $('.menu.item'),
+        label: $('.menu.item h1'),
+        content: $('.menu.content'),
+        offset: [
+            $('#profile').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52),
+            $('#services').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52),
+            $('#network').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52),
+            $('#originals').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52),
+            $('#experiential').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52),
+            $('#connect').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52)
+        ],
+        index: 0
+    };
+		_articles = $('#network-content .carousel-inner'),
+		url = '';
+	for (let i = 0; i < 15; i++) {
+		switch (i) {
+			case 0:
+				url = 'https://www.hollywoodreporter.com/news/netflix-acquires-filipino-rom-girl-allergic-wifi-1229889';
+				break;
+			case 1:
+				url = 'https://variety.com/2019/tv/asia/hbo-asia-renews-halfworlds-third-season-series-in-philippines-1203419572/';
+				break;
+			case 2:
+				url = 'https://decider.com/2018/04/09/amo-netflix-review/';
+				break;
+			case 3:
+				url = 'https://screenanarchy.com/2016/11/seclusion-trailer-in-erik-matti.html';
+				break;
+			case 4:
+				url = 'https://www.economist.com/asia/2015/11/21/she-once-was-lost';
+				break;
+			case 5:
+				url = 'https://www.imdb.com/news/ni62702733';
+				break;
+			case 6:
+				url = 'https://tv5.espn.com/story/_/id/28258103/sibol-dota-2-team-gives-ph-another-gold-medal-esports';
+				break;
+			case 7:
+				url = 'https://onenews.ph/phl-esports-bets-seen-to-lead-in-sea-games';
+				break;
+			case 8:
+				url = 'https://news.abs-cbn.com/sports/12/08/19/sea-games-pinoy-mobile-legends-team-wins-first-esports-gold';
+				break;
+			case 9:
+				url = 'https://www.gmanetwork.com/news/showbiz/chikaminute/704503/rocco-nacino-topbills-new-movie-write-about-love/story/';
+				break;
+			case 10:
+				url = 'https://www.rappler.com/entertainment/music/244519-yeng-constantino-ikaw-ang-akin-write-about-love';
+				break;
+			case 11:
+				url = 'https://sports.inquirer.net/379065/uncanny-heroes';
+				break;
+			case 12:
+				url = 'https://www.preview.ph/beauty/5-derma-approved-ways-to-look-like-a-star-adv-con-pr';
+				break;
+			case 13:
+				url = 'https://www.spin.ph/life/guide/sea-games-viewing-guide-a1374-20191119';
+				break;
+			case 14:
+				url = 'https://www.esquiremag.ph/money/capital/c-estates-is-poised-to-democratize-the-real-estate-market-a1926-20190716';
+		}
+		var index, href;
+		$(_articles).append(`<div class="carousel-item ${0 == i ? 'active' : ''}"><a href=${(href = url)} target="_blank"><img src="{{ asset('images/articles/a${(index = i)}.jpg') }}" width="80%" alt="..."></a></div>`);
+	}
+	$(control.menu).on('click', function (event) {
+		(control.index = control.menu.index(this)), closeAllMenu(control, control.index), 3 != control.index && toggleMenu(control.offset[control.index], control.label[control.index], control.content[control.index]);
+	});
+}),
+	$(window).on('load', function () {
+		var urlParams;
+		switch (new URLSearchParams(window.location.search).get('page')) {
+			case 'profile':
+				openMenu($('#profile').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52), $('#profile h1'), $('#profile-content'));
+				break;
+			case 'services':
+				openMenu($('#services').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52), $('#services h1'), $('#services-content'));
+				break;
+			case 'network':
+				openMenu($('#network').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52), $('#network h1'), $('#network-content'));
+				break;
+			case 'originals':
+				openMenu($('#originals').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52), $('#originals h1'), $('#originals-content'));
+				break;
+			case 'experiential':
+				openMenu($('#experiential').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52), $('#experiential h1'), $('#experiential-content'));
+				break;
+			case 'connect':
+				openMenu($('#connect').offset().top - (getCurrentMedia() == 'mobile' ? 44 : 52), $('#connect h1'), $('#connect-content'));
+				break;
+			default:
+				$(window).scrollTop(0);
+		}
+	});
+
 </script>
 @endsection
