f7999bf840ba4fa1dd46b4976bca82a5443f9676
tony <tony.tripulca@gmail.com>
2020-04-22 13:20:19 +0800

    #43 Adjusted preloader letter spacing (desktop)

--- application/resources/views/layouts/web/app.blade.php
+++ application/resources/views/layouts/web/app.blade.php
@@ -66,8 +66,8 @@
 <body> 
     <div class="loader">
         <div class="holder d-flex align-items-center justify-content-center">
-            <p class="lead-1 couture-bold text-25 text-md-40 text-lg-60 space-07">PUBLICITY</p>
-            <p class="lead-2 couture-bold text-25 text-md-40 text-lg-60 space-07">ASIA</p>
+            <p class="lead-1 couture-bold text-25 text-md-40 text-lg-60 space-07 space-lg-05">PUBLICITY</p>
+            <p class="lead-2 couture-bold text-25 text-md-40 text-lg-60 space-07 space-lg-05">ASIA</p>
         </div>
     </div>
 
