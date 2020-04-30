f06fba375937c83c3f98e15282f8d04b90192920
tony <tony.tripulca@gmail.com>
2020-05-01 07:38:25 +0800

    Accordion menu using jquery

--- application/public/js/web/menu.js
+++ application/public/js/web/menu.js
@@ -1,42 +1,47 @@
 // Globals
-var control = {
-	menu: {
+var 
+	menu = {
 		el: [],
 		offset: [],
 		height: [],
 	},
-	label: [],
-	content: {
+	button = {
+		el: null,
+		label: null
+	},
+	content = {
 		el: [],
 		offset: [],
 		height: [],
-	},
-	index: 0,
-};
+	};
 
 $(() => {
-	init();
+	initMenu();
 
-	$(window).on("scroll", function () {
+	$(".menu-button").on("click", function () {
+		var index = $(".menu-button").index(this);
 
+		$(this).toggleClass('active');
+
+		if($(this).hasClass('active')) {
+			$(content.el[index]).slideUp();
+		} else {
+			$(content.el[index]).slideDown();
+		}
 	});
 });
 
-$(window).on("load", function () {
+$(window).on("load", function () {});
 
-});
-
-function init() {
-
+function initMenu() {
+	content.el = $("div[class*=menu-content]");
 }
 
 function getCurrentMedia() {
-	if(window.matchMedia("(min-width: 992px)").matches) {
+	if (window.matchMedia("(min-width: 992px)").matches) {
 		return "desktop";
-
-	} else if(window.matchMedia("(min-width: 768px)").matches){
+	} else if (window.matchMedia("(min-width: 768px)").matches) {
 		return "tablet";
-
 	} else {
 		return "mobile";
 	}
@@ -49,49 +54,61 @@ function setArticles() {
 	for (let i = 0; i < 15; i++) {
 		switch (i) {
 			case 0:
-				url = "https://www.hollywoodreporter.com/news/netflix-acquires-filipino-rom-girl-allergic-wifi-1229889";
+				url =
+					"https://www.hollywoodreporter.com/news/netflix-acquires-filipino-rom-girl-allergic-wifi-1229889";
 				break;
 			case 1:
-				url = "https://variety.com/2019/tv/asia/hbo-asia-renews-halfworlds-third-season-series-in-philippines-1203419572/";
+				url =
+					"https://variety.com/2019/tv/asia/hbo-asia-renews-halfworlds-third-season-series-in-philippines-1203419572/";
 				break;
 			case 2:
 				url = "https://decider.com/2018/04/09/amo-netflix-review/";
 				break;
 			case 3:
-				url = "https://screenanarchy.com/2016/11/seclusion-trailer-in-erik-matti.html";
+				url =
+					"https://screenanarchy.com/2016/11/seclusion-trailer-in-erik-matti.html";
 				break;
 			case 4:
-				url = "https://www.economist.com/asia/2015/11/21/she-once-was-lost";
+				url =
+					"https://www.economist.com/asia/2015/11/21/she-once-was-lost";
 				break;
 			case 5:
 				url = "https://www.imdb.com/news/ni62702733";
 				break;
 			case 6:
-				url = "https://tv5.espn.com/story/_/id/28258103/sibol-dota-2-team-gives-ph-another-gold-medal-esports";
+				url =
+					"https://tv5.espn.com/story/_/id/28258103/sibol-dota-2-team-gives-ph-another-gold-medal-esports";
 				break;
 			case 7:
-				url = "https://onenews.ph/phl-esports-bets-seen-to-lead-in-sea-games";
+				url =
+					"https://onenews.ph/phl-esports-bets-seen-to-lead-in-sea-games";
 				break;
 			case 8:
-				url = "https://news.abs-cbn.com/sports/12/08/19/sea-games-pinoy-mobile-legends-team-wins-first-esports-gold";
+				url =
+					"https://news.abs-cbn.com/sports/12/08/19/sea-games-pinoy-mobile-legends-team-wins-first-esports-gold";
 				break;
 			case 9:
-				url = "https://www.gmanetwork.com/news/showbiz/chikaminute/704503/rocco-nacino-topbills-new-movie-write-about-love/story/";
+				url =
+					"https://www.gmanetwork.com/news/showbiz/chikaminute/704503/rocco-nacino-topbills-new-movie-write-about-love/story/";
 				break;
 			case 10:
-				url = "https://www.rappler.com/entertainment/music/244519-yeng-constantino-ikaw-ang-akin-write-about-love";
+				url =
+					"https://www.rappler.com/entertainment/music/244519-yeng-constantino-ikaw-ang-akin-write-about-love";
 				break;
 			case 11:
 				url = "https://sports.inquirer.net/379065/uncanny-heroes";
 				break;
 			case 12:
-				url = "https://www.preview.ph/beauty/5-derma-approved-ways-to-look-like-a-star-adv-con-pr";
+				url =
+					"https://www.preview.ph/beauty/5-derma-approved-ways-to-look-like-a-star-adv-con-pr";
 				break;
 			case 13:
-				url = "https://www.spin.ph/life/guide/sea-games-viewing-guide-a1374-20191119";
+				url =
+					"https://www.spin.ph/life/guide/sea-games-viewing-guide-a1374-20191119";
 				break;
 			case 14:
-				url = "https://www.esquiremag.ph/money/capital/c-estates-is-poised-to-democratize-the-real-estate-market-a1926-20190716";
+				url =
+					"https://www.esquiremag.ph/money/capital/c-estates-is-poised-to-democratize-the-real-estate-market-a1926-20190716";
 				break;
 		}
 		$(_articles).append(
--- application/resources/views/web/landing/menu.blade.php
+++ application/resources/views/web/landing/menu.blade.php
@@ -28,16 +28,30 @@
 
 @section('content')
 <section>
-	<ul class="menu">
-		<li class="item text-20 text-white">
-			<h1 class="title bg-gray-1">PROFILE</h1>
+	<div class="menu">
+		<div class="menu-button bg-gray-1">
+			<h1 class="title">PROFILE</h1>
+		</div>
+		<div class="menu-content">
 			<p>Cupidatat in voluptate fugiat ipsum. Ut nulla sint ex tempor Lorem exercitation commodo nulla voluptate in adipisicing qui officia. Id ea incididunt occaecat est sunt tempor magna qui labore. Nostrud eiusmod duis nisi amet. Eiusmod laborum id laborum ea. Proident officia incididunt occaecat culpa. Enim voluptate tempor sit do occaecat pariatur Lorem adipisicing.</p>
 			<p>Excepteur Lorem consectetur tempor est. Lorem incididunt ut ut exercitation reprehenderit est nostrud qui. Consequat reprehenderit qui ex duis sunt ut incididunt sit Lorem non aliqua in occaecat. Irure cupidatat sit laborum irure proident velit eu tempor non ea ex in aliquip sit. Voluptate cillum exercitation nisi laboris est voluptate consequat dolor nostrud cillum in. Proident deserunt nisi nulla ea ut laborum nostrud ut voluptate et ea consectetur velit officia. Reprehenderit et ullamco nostrud ut.</p>
-		</li>
-		<li class="item text-20 text-white">
-			<h1 class="title bg-gray-2">PROFILE</h1>
+		</div>
+		<div class="services menu-button bg-gray-2">
+			<h1 class="title">SERVICES</h1>
+		</div>
+		<div class="services menu-content">
 			<p>Cupidatat in voluptate fugiat ipsum. Ut nulla sint ex tempor Lorem exercitation commodo nulla voluptate in adipisicing qui officia. Id ea incididunt occaecat est sunt tempor magna qui labore. Nostrud eiusmod duis nisi amet. Eiusmod laborum id laborum ea. Proident officia incididunt occaecat culpa. Enim voluptate tempor sit do occaecat pariatur Lorem adipisicing.</p>
-		</li>
-	</ul>
+			<p>Excepteur Lorem consectetur tempor est. Lorem incididunt ut ut exercitation reprehenderit est nostrud qui. Consequat reprehenderit qui ex duis sunt ut incididunt sit Lorem non aliqua in occaecat. Irure cupidatat sit laborum irure proident velit eu tempor non ea ex in aliquip sit. Voluptate cillum exercitation nisi laboris est voluptate consequat dolor nostrud cillum in. Proident deserunt nisi nulla ea ut laborum nostrud ut voluptate et ea consectetur velit officia. Reprehenderit et ullamco nostrud ut.</p>
+			<p>Cupidatat in voluptate fugiat ipsum. Ut nulla sint ex tempor Lorem exercitation commodo nulla voluptate in adipisicing qui officia. Id ea incididunt occaecat est sunt tempor magna qui labore. Nostrud eiusmod duis nisi amet. Eiusmod laborum id laborum ea. Proident officia incididunt occaecat culpa. Enim voluptate tempor sit do occaecat pariatur Lorem adipisicing.</p>
+			<p>Excepteur Lorem consectetur tempor est. Lorem incididunt ut ut exercitation reprehenderit est nostrud qui. Consequat reprehenderit qui ex duis sunt ut incididunt sit Lorem non aliqua in occaecat. Irure cupidatat sit laborum irure proident velit eu tempor non ea ex in aliquip sit. Voluptate cillum exercitation nisi laboris est voluptate consequat dolor nostrud cillum in. Proident deserunt nisi nulla ea ut laborum nostrud ut voluptate et ea consectetur velit officia. Reprehenderit et ullamco nostrud ut.</p>
+		</div>
+		<div class="menu-button bg-gray-3">
+			<h1 class="title">NETWORK</h1>
+		</div>
+		<div class="menu-content">
+			<p>Cupidatat in voluptate fugiat ipsum. Ut nulla sint ex tempor Lorem exercitation commodo nulla voluptate in adipisicing qui officia. Id ea incididunt occaecat est sunt tempor magna qui labore. Nostrud eiusmod duis nisi amet. Eiusmod laborum id laborum ea. Proident officia incididunt occaecat culpa. Enim voluptate tempor sit do occaecat pariatur Lorem adipisicing.</p>
+			<p>Excepteur Lorem consectetur tempor est. Lorem incididunt ut ut exercitation reprehenderit est nostrud qui. Consequat reprehenderit qui ex duis sunt ut incididunt sit Lorem non aliqua in occaecat. Irure cupidatat sit laborum irure proident velit eu tempor non ea ex in aliquip sit. Voluptate cillum exercitation nisi laboris est voluptate consequat dolor nostrud cillum in. Proident deserunt nisi nulla ea ut laborum nostrud ut voluptate et ea consectetur velit officia. Reprehenderit et ullamco nostrud ut.</p>
+		</div>
+	</div>
 </section>
 @endsection
